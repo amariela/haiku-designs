4f1caea9702d42008faa419086b47f19920e5863
amariela <teodocio.am@gmail.com>
2025-03-24 12:09:19 -0500

    feat: seed db with dining category products

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@ require "faker"
 # Category data
 # Category.destroy_all
 
-# Category.create(name: "Storage")
 # Category.create(name: "Home")
 # Category.create(name: "Office")
 # Category.create(name: "Outdoor")
+# Category.create(name: "Dining")
 
 # puts "Created a total of #{Category.count} categories."
 
@@ -22,60 +22,60 @@ def proper_price(price)
 end
 
 # HOME category products
-# home_category = Category.find_by(name: "Home")
+home_category = Category.find_by(name: "Home")
 
-# home_url = "https://luxefurniture.ca/collections/living-room?view=view-36&grid_list=grid-view"
-# home_response = HTTParty.get(home_url)
+home_url = "https://luxefurniture.ca/collections/living-room?view=view-36&grid_list=grid-view"
+home_response = HTTParty.get(home_url)
 
-# if home_response.code == 200
-#     home_parsed_page = Nokogiri::HTML(home_response.body)
-#     home_products = home_parsed_page.css('div.productitem--info')
-#     home_count = 0
-#     home_products.each do |product|
-#         if home_count == 25
-#             break
-#         end
-#         name = product.css('h2.productitem--title > a')
-#         price = product.css('div.price--main > span.money')
-#         home_category.products.create(
-#             name: name.text.strip,
-#             description: Faker::Marketing.buzzwords,
-#             price: proper_price(price.text.strip),
-#             stock_quantity: Faker::Number.number(digits: 3)
-#         )
-#         home_count += 1
-#     end
-#     home_products = Product.where(category_id: home_category.id)
-#     puts "Created a total of #{home_products.count} home products."
-# end
+if home_response.code == 200
+    home_parsed_page = Nokogiri::HTML(home_response.body)
+    home_products = home_parsed_page.css('div.productitem--info')
+    home_count = 0
+    home_products.each do |product|
+        if home_count == 25
+            break
+        end
+        name = product.css('h2.productitem--title > a')
+        price = product.css('div.price--main > span.money')
+        home_category.products.create(
+            name: name.text.strip,
+            description: Faker::Marketing.buzzwords,
+            price: proper_price(price.text.strip),
+            stock_quantity: Faker::Number.number(digits: 3)
+        )
+        home_count += 1
+    end
+    home_products = Product.where(category_id: home_category.id)
+    puts "Created a total of #{home_products.count} home products."
+end
 
 # OUTDOOR category products
-# outdoor_category = Category.find_by(name: "Outdoor")
+outdoor_category = Category.find_by(name: "Outdoor")
 
-# outdoor_url = "https://luxefurniture.ca/collections/outdoor-dining-tables?view=view-36&grid_list=grid-view"
-# outdoor_response = HTTParty.get(outdoor_url)
+outdoor_url = "https://luxefurniture.ca/collections/outdoor-dining-tables?view=view-36&grid_list=grid-view"
+outdoor_response = HTTParty.get(outdoor_url)
 
-# if outdoor_response.code == 200
-#     outdoor_parsed_page = Nokogiri::HTML(outdoor_response.body)
-#     outdoor_products = outdoor_parsed_page.css('div.productitem--info')
-#     outdoor_count = 0
-#     outdoor_products.each do |product|
-#         if outdoor_count == 25
-#             break
-#         end
-#         outdoor_name = product.css('h2.productitem--title > a')
-#         outdoor_price = product.css('div.price--main > span.money')
-#         outdoor_category.products.create(
-#             name: outdoor_name.text.strip,
-#             description: Faker::Marketing.buzzwords,
-#             price: proper_price(outdoor_price.text.strip),
-#             stock_quantity: Faker::Number.number(digits: 3)
-#         )
-#         outdoor_count += 1
-#     end
-#     outdoor_products = Product.where(category_id: outdoor_category.id)
-#     puts "Created a total of #{outdoor_products.count} outdoor products."
-# end
+if outdoor_response.code == 200
+    outdoor_parsed_page = Nokogiri::HTML(outdoor_response.body)
+    outdoor_products = outdoor_parsed_page.css('div.productitem--info')
+    outdoor_count = 0
+    outdoor_products.each do |product|
+        if outdoor_count == 25
+            break
+        end
+        outdoor_name = product.css('h2.productitem--title > a')
+        outdoor_price = product.css('div.price--main > span.money')
+        outdoor_category.products.create(
+            name: outdoor_name.text.strip,
+            description: Faker::Marketing.buzzwords,
+            price: proper_price(outdoor_price.text.strip),
+            stock_quantity: Faker::Number.number(digits: 3)
+        )
+        outdoor_count += 1
+    end
+    outdoor_products = Product.where(category_id: outdoor_category.id)
+    puts "Created a total of #{outdoor_products.count} outdoor products."
+end
 
 # OFFICE category products
 office_category = Category.find_by(name: "Office")
@@ -103,4 +103,33 @@ if office_response.code == 200
     end
     office_products = Product.where(category_id: office_category.id)
     puts "Created a total of #{office_products.count} office products."
+end
+
+
+# DINING category products
+dining_category = Category.find_by(name: "Dining")
+
+dining_url = "https://luxefurniture.ca/collections/diningroom"
+dining_response = HTTParty.get(dining_url)
+
+if dining_response.code == 200
+    dining_parsed_page = Nokogiri::HTML(dining_response.body)
+    dining_products = dining_parsed_page.css('div.productitem--info')
+    dining_count = 0
+    dining_products.each do |product|
+        if dining_count == 40
+            break
+        end
+        dining_name = product.css('h2.productitem--title > a')
+        dining_price = product.css('div.price--main > span.money')
+        dining_category.products.create(
+            name: dining_name.text.strip,
+            description: Faker::Marketing.buzzwords,
+            price: proper_price(dining_price.text.strip),
+            stock_quantity: Faker::Number.number(digits: 2)
+        )
+        dining_count += 1
+    end
+    dining_products = Product.where(category_id: dining_category.id)
+    puts "Created a total of #{dining_products.count} dining products."
 end
