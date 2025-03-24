7cb2f0a9618f96de4afbdfa6bb27f12b29822888
amariela <teodocio.am@gmail.com>
2025-03-24 11:40:49 -0500

    feat: seed db with outdoor category products

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,31 +21,58 @@ def proper_price(price)
     price.gsub(/[$,]/, '').to_f
 end
 
-# Home category products
-home_category = Category.find_by(name: "Home")
+# HOME category products
+# home_category = Category.find_by(name: "Home")
 
-home_url = "https://luxefurniture.ca/collections/living-room?view=view-36&grid_list=grid-view"
-home_response = HTTParty.get(home_url)
+# home_url = "https://luxefurniture.ca/collections/living-room?view=view-36&grid_list=grid-view"
+# home_response = HTTParty.get(home_url)
 
-if home_response.code == 200
-    home_parsed_page = Nokogiri::HTML(home_response.body)
-    home_products = home_parsed_page.css('div.productitem--info')
-    count = 0
-    home_products.each do |product|
-        if count == 25
+# if home_response.code == 200
+#     home_parsed_page = Nokogiri::HTML(home_response.body)
+#     home_products = home_parsed_page.css('div.productitem--info')
+#     home_count = 0
+#     home_products.each do |product|
+#         if home_count == 25
+#             break
+#         end
+#         name = product.css('h2.productitem--title > a')
+#         price = product.css('div.price--main > span.money')
+#         home_category.products.create(
+#             name: name.text.strip,
+#             description: Faker::Marketing.buzzwords,
+#             price: proper_price(price.text.strip),
+#             stock_quantity: Faker::Number.number(digits: 3)
+#         )
+#         home_count += 1
+#     end
+#     home_products = Product.where(category_id: home_category.id)
+#     puts "Created a total of #{home_products.count} home products."
+# end
+
+# OUTDOOR category products
+outdoor_category = Category.find_by(name: "Outdoor")
+
+outdoor_url = "https://luxefurniture.ca/collections/outdoor-dining-tables?view=view-36&grid_list=grid-view"
+outdoor_response = HTTParty.get(outdoor_url)
+
+if outdoor_response.code == 200
+    outdoor_parsed_page = Nokogiri::HTML(outdoor_response.body)
+    outdoor_products = outdoor_parsed_page.css('div.productitem--info')
+    outdoor_count = 0
+    outdoor_products.each do |product|
+        if outdoor_count == 25
             break
         end
-        name = product.css('h2.productitem--title > a')
-        price = product.css('div.price--main > span.money')
-        home_category.products.create(
-            name: name.text.strip,
+        outdoor_name = product.css('h2.productitem--title > a')
+        outdoor_price = product.css('div.price--main > span.money')
+        outdoor_category.products.create(
+            name: outdoor_name.text.strip,
             description: Faker::Marketing.buzzwords,
-            price: proper_price(price.text.strip),
+            price: proper_price(outdoor_price.text.strip),
             stock_quantity: Faker::Number.number(digits: 3)
         )
-        count += 1
+        outdoor_count += 1
     end
-    products = Product.where(category_id: home_category.id)
-    puts "Created a total of #{products.count} products."
+    outdoor_products = Product.where(category_id: outdoor_category.id)
+    puts "Created a total of #{outdoor_products.count} outdoor products."
 end
-
