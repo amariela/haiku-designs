c3d2a526d1f7c351caa5a08ab07f72b0f285ba4e
Ela <teodocio.am@gmail.com>
2025-03-25 13:21:18 -0500

    feat: display product information in products#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
 
   # GET /products/1 or /products/1.json
   def show
+    @category = Category.find_by(id: @product.category_id)
   end
 
   # GET /products/new
