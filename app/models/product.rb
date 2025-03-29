d45a042f07883deefa5f0e85f8d28468712aa8df
Ela <teodocio.am@gmail.com>
2025-03-28 19:41:24 -0500

    feat: add categories management

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   belongs_to :category
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "name", "price", "description", "stock_quantity", "created_at", "updated_at" ]
+    [ "id", "id_value", "name", "price", "description", "stock_quantity", "created_at", "updated_at" ]
   end
 
   def self.ransackable_associations(auth_object = nil)
