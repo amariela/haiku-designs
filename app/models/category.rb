836c86f4cea60b87bf01c251ad1930ecd8ced152
Ela <teodocio.am@gmail.com>
2025-03-28 19:35:07 -0500

    feat: allow admin to update product listings

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   validates :name, presence: true
   has_many :products, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "id", "id_value", "name", "updated_at" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "products" ]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
   validates :name, :price, :stock_quantity, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :category
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "name", "price", "description", "stock_quantity", "created_at", "updated_at" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "category" ]
+  end
 end
